848d583b10bde2c43a9f0b06f86fc6f9b4fced2f
DraganKojic <dkojic325@gmail.com>
2021-08-17 21:42:22 +1000

    Add levelUp (payable) function

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -4,9 +4,16 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
+  uint levelUpFee = 0.001 ether;
+
   modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
     _;
+  }
+
+  function levelUp(uint _zombieId) external payable {
+    require(msg.value == levelUpFee);
+    zombies[_zombieId].level++;
   }
 
   function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
